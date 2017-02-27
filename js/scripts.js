e89316250a0b6fdf93113138852498e1a6a843b5
paul sparks <paulnderitu17@gmail.com>
2017-02-27 15:35:52 +0300

    adding updated to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
   this.firstName=first;
   this.lastName=last;
 }
+Contact.prototype.fullName=function() {
+  return this.firstName + " " + this.lastName;
+};
 
 //front end logic
 $(document).ready(function() {
@@ -12,7 +15,7 @@ $(document).ready(function() {
     var inputtedLastName=$("input#new-last-name").val();
 
     var newContact=new Contact(inputtedFirstName, inputtedLastName);
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " "+ newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
     $(".contact").last().click(function() {
         $("#show-contact").show();
         $("#show-contact h2").text(newContact.firstName);
